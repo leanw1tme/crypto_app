c2d31875b531d9fd301b2783669e13a14fed7480
PLeVeL1857 <smagulovabilai7@gmail.com>
2023-07-14 06:16:23 +0600

    revolution

---
+++ lib/models/data/roi_data.dart
@@ -0,0 +1,32 @@
+import 'package:crypto_app/models/data/sparkline_data.dart';
+
+import 'main_data.dart';
+
+class Roi {
+  double times;
+  Currency currency;
+  double percentage;
+
+  Roi({
+    required this.times,
+    required this.currency,
+    required this.percentage,
+  });
+
+  factory Roi.fromJson(Map<String, dynamic> json) => Roi(
+        times: json["times"]?.toDouble(),
+        currency: currencyValues.map[json["currency"]]!,
+        percentage: json["percentage"]?.toDouble(),
+      );
+
+  Map<String, dynamic> toJson() => {
+        "times": times,
+        "currency": currencyValues.reverse[currency],
+        "percentage": percentage,
+      };
+}
+
+enum Currency { BTC, USD, ETH }
+
+final currencyValues =
+    EnumValues({"btc": Currency.BTC, "eth": Currency.ETH, "usd": Currency.USD});
